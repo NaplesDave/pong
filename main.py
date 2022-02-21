f73e711a337468fd8a726e82dec8c45cdfe45ec3
NaplesDave <k2_daveking@hotmail.com>
2022-02-21 16:09:25 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+# Pong Arcade game project, London App Brewery Course on UDEMY.
+# began Feb 21, 2022 David A. King, Naples, FL USA
+screen = Screen()
+screen.setup(width=800, height=600)
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
